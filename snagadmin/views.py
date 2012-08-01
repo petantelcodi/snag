caa938730148c305ba2c423808f9db96e7f19954
Jaume <jaume@nualart.cat>
2012-08-02 00:07:16 +0200

    Starting to build admin interface

--- snagadmin/views.py
+++ snagadmin/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from random import randint, shuffle, random, seed
 from snag.genview import models
 from sys import *
-from snag.genview.models import Gens, Contents, Tasks
+from snag.genview.models import Gens, Contents, Tasks, Chromosome
 from django.utils.encoding import smart_str, smart_unicode
 import math
 
@@ -13,11 +13,18 @@ import math
 # Home page
 def main(request):
     # auth
-    if request.user.is_authenticated():
-        auth = "<p>User is authenticated as: <b>"+request.user.username+"</b></p>"
+    if request.user.is_authenticated() and request.user.username == "admin":
+        auth = "<p>Welcome <b>"+request.user.username+"</b></p>"
+
+        # Getting and reordering Tasks
+        tasksList = []
+        for x in Tasks.objects.all():
+            tasksList.append([smart_str(x.user_id), smart_str(x.chromosome_id_id), smart_str(x.test_date), smart_str(x.total_test_time), smart_str(x.test_ok) ])
+        #for
 
     else:
-        auth = "<p>User is anonymous</p><p>Instructions for anonymous users here.</p>"
+        auth = "<p>This page is only accessible for admin users</p>"
+        tasksList = []
 
-    return render_to_response('main.html', {'auth': auth})
+    return render_to_response('main.html', {'auth': auth, 'tasksList': tasksList})
 

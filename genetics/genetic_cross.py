c8e0a45dce938d4423b742c4629229b4332166f5
mcanet <mar.canet@gmail.com>
2012-09-17 03:04:33 +0200

    add new cross methods

--- genetics/genetic_cross.py
+++ genetics/genetic_cross.py
@@ -1,14 +1,14 @@
-class genetic_cross{
-	def __init__(self, father, mother, type):
-		self.totalGens = 27 # or 32
+class genetic_cross:
+	def __init__(self, father, mother, type, totalGens):
+		self.totalGens = totalGens
 		self.father = father
 		self.mother = mother
 		self.son = self.father
 		if type=="pure":
 			self.pureCross()
-		else if type=="same_level":
+		elif type=="same_level":
 			self.sameLevelCross()
-		else if type=="inferior_level":
+		elif type=="inferior_level":
 			self.sameLevelCross()
 	
 	def deleteArrayDeptherThan4Levels(self):
@@ -19,12 +19,16 @@ class genetic_cross{
 	def randomGens(self):
 		iFatherGen = 0
         iMotherGen = 0
-		while iFatherGen !=iMotherGen and len(p)>2:
+        while iFatherGen !=iMotherGen and len(p)>2:
                 iFather = randrange(0,self.totalGens)
                 iMother = randrange(0,self.totalGens)
                 # father lower number because is the better gen
                 if self.father[iFather]["id"]!=self.mother[iMother]["id"]:
-                	return [iFather,iMother]
+                	l = list()
+                	l.append(iFather)
+                	l.append(iMother)
+                	break
+                	return l
                 	
 	def repetedGens(self):
 		print repeatedGens
@@ -33,8 +37,8 @@ class genetic_cross{
 	def getGen(self):
 		print "getGens"
     
-    def shiftFatherToMotherGens(self,idGen):
-    	print "shiftFatherToMotherGens"            	
+	def shiftFatherToMotherGens(self,idGen):
+		print "shiftFatherToMotherGens"            	
 		# 1. 
 		#self.son
 		
@@ -58,5 +62,9 @@ class genetic_cross{
 	def inferiorLevelCross(self):
 		print "inferior level cross"
 		rGens = self.randomGens()
-}
 
+if __name__=="__main__":
+	father = [{'id': 1, 'child': [{'id': 5, 'child': [{'id': 4, 'child': []}]}]},{'id': 2, 'child': []},{'id': 3, 'child': []}]
+	mother = [{'id': 2, 'child': [{'id': 4, 'child': [{'id': 5, 'child': []}]}]},{'id': 1, 'child': []},{'id': 3, 'child': []}]
+	g = genetic_cross(father,mother,"pure",5)
+	print g
